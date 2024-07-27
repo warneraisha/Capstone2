54a43398e8006dbff23498b3f6011f91ab78e1b3
Bhavsagar <bhavsagar@protonmail.com>
2024-07-27 11:14:49 +0530

    Added search funtionality

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,92 +4,94 @@ import ProductCard from './ProductCard';
 
 let Home = () => {
   let [ProductArray, setProductArray] = useState([...ProductData]);
-
-  // let topRatedData = [];
+  let [searchText, setSearchText] = useState('');
 
   let filterTopRated = () => {
     let data = ProductArray.filter((obj) => {
       return obj.ratings > 4;
     });
-
     setProductArray(data);
   };
 
   let filterProduct = (ProCategory) => {
-    let data = ProductArray.filter((obj) => {
-      return ProCategory == obj.category;
+    let data = ProductData.filter((obj) => {
+      return ProCategory === obj.category;
     });
     setProductArray(data);
   };
 
+  let searchProduct = () => {
+    let data = ProductData.filter((obj) =>
+      obj.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setProductArray(data);
+  };
+
   return (
-    <div className=" border-2 border-red-600 bg-white ">
+    <div className="border-2 border-red-600 bg-white">
       <div className="utility flex justify-center m-5">
-        <div className="search-box w-1/2">
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered bg-white text-black w-full max-w-xs"
-          />
-          <button className="btn btn-accent ml-2">Search</button>
-        </div>
-        <button className="btn ml-2" onClick={filterTopRated}>
+        <button
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={filterTopRated}
+        >
           Top Rated
         </button>
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct("Men's Sneaker");
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct("Men's Sneaker")}
         >
           Men's Sneaker
         </button>
-
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct("Men's Pants");
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct("Men's Pants")}
         >
           Men's Pants
         </button>
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct("Men's Boot");
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct("Men's Boot")}
         >
           Men's Boot
         </button>
+
+        <div className="search-box w-1/2 ml-20">
+          <input
+            value={searchText}
+            onChange={(event) => setSearchText(event.target.value)}
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered border-red-500 bg-white text-black w-full max-w-xs"
+          />
+          <button
+            className="btn btn-accent ml-2 border-2 hover:border-red-500"
+            onClick={searchProduct}
+          >
+            Search
+          </button>
+        </div>
+
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct('Bag');
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct('Bag')}
         >
           Bag
         </button>
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct('Cap');
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct('Cap')}
         >
           Cap
         </button>
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct('Bottle');
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct('Bottle')}
         >
           Bottle
         </button>
         <button
-          className="btn ml-2"
-          onClick={() => {
-            filterProduct('Earphones');
-          }}
+          className="btn ml-2 border-2 hover:border-red-500"
+          onClick={() => filterProduct('Earphones')}
         >
           Earphones
         </button>
--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -2,9 +2,9 @@ let ProductCard = ({ obj }) => {
   let { name, seller, price, ratings, category, img } = obj;
 
   return (
-    <div className="card mt-6">
+    <div className="card mt-6 ">
       <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="">
+        <figure>
           <img src={img} alt="Shoes" />
         </figure>
         <div className="card-body">
@@ -12,7 +12,7 @@ let ProductCard = ({ obj }) => {
             {name}
             <div className="badge badge-secondary">{category}</div>
           </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+
           <div className="card-actions justify-end">
             <div className="badge badge-outline">{ratings}</div>
             <div className="badge badge-outline">${price}</div>
